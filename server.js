42fb7e74e6a159f875970b8bca1ef1b7bc60e9be
Joe <joe@dyn-118-138-20-1.its.monash.edu.au>
2018-07-21 18:38:43 +1000

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 app.use(express.static('./dist/flickr-feed-angular'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('./dist/flickr-feed-angular/index.html'));
+  res.sendFile("index.html", {
+    "root": __dirname
+  });
 });
 app.listen(process.env.PORT || 8080);
